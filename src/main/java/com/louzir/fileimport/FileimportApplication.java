ab350fb1a0b6f7e533323b274bb3a15b91eba09f
Mohamed Aziz LOUZIR <medaziz.louzir@gmail.com>
2024-03-17 02:44:41 +0100

    first commit

---
+++ src/main/java/com/louzir/fileimport/FileimportApplication.java
@@ -0,0 +1,13 @@
+package com.louzir.fileimport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileimportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileimportApplication.class, args);
+	}
+
+}
